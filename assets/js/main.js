ac2655d2ae64da529c845ce5f801d006ebd074d9
Ewann Pelle <ewannpv@chromium.org>
2022-11-21 13:42:35 +0100

    Move data to a separate file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,3 @@
-// ======== Data Section ========
-
-const songs = [
-  {
-    title: "Piscine de fleurs",
-    artist: "Call Me At Night",
-    cover_img_name: "song_01.jpg",
-    availability_date: "",
-    spotify_link: "https://open.spotify.com/",
-  },
-  {
-    title: "Piscine de fleurs",
-    artist: "Call Me At Night",
-    cover_img_name: "song_01.jpg",
-    availability_date: "NOVEMBER 25, 2022",
-    spotify_link: "",
-  },
-];
-
-// ======== Script Section ========
 
 const populateCarousel = () => {
   for (let index = 0; index < songs.length; index++) {
