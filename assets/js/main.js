a8556796465b5d900ad24dbd915fb10bb3f25619
Ewann Pelle <ewannpv@chromium.org>
2022-11-21 13:27:34 +0100

    Add fake song

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,13 @@ const songs = [
     title: "Piscine de fleurs",
     artist: "Call Me At Night",
     cover_img_name: "song_01.jpg",
+    availability_date: "",
+    spotify_link: "https://open.spotify.com/",
+  },
+  {
+    title: "Piscine de fleurs",
+    artist: "Call Me At Night",
+    cover_img_name: "song_01.jpg",
     availability_date: "NOVEMBER 25, 2022",
     spotify_link: "",
   },
