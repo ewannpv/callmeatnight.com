8cda95d03b5ca78f7269371971dc97045d4ded6b
Ewann Pelle <ewannpv@chromium.org>
2022-11-21 14:27:53 +0100

    Update gtag events and clean populate_carousel.js

---
+++ assets/js/populate_carousel.js
@@ -0,0 +1,59 @@
+const populateCarousel = () => {
+  for (let index = 0; index < songs.length; index++) {
+    let songHeader = `
+    <div class="carousel-item h-100 ${index == 0 ? `active` : ``}">
+    <div class="row h-100 align-items-center">
+      <div class="col text-center">
+      `;
+
+    let songCover = `
+    <img
+    src="${`./assets/img/songs/${songs[index].cover_img_name}`}"
+    class="carousel_song_cover center"
+    alt=""
+    />`;
+
+    let songLink = songs[index].spotify_link
+      ? `
+      <div class="carousel_song_link">
+      <a 
+        href="${songs[index].spotify_link}"
+        target="_blank"
+        onclick="gtag('event', 'open_song_${songs[index].title}', {'event_category': 'sound_carousel', 'event_label': '${songs[index].title}'});"
+      >
+        <i class="fa-brands fa-spotify carousel_spotify_logo_available"></i>
+      </a>
+    </div>
+    `
+      : `
+    <div class="carousel_song_link">
+      <i class="fa-brands fa-spotify carousel_spotify_logo_not_available"></i>
+    </div>`;
+
+    let songInfo = `
+    <div class="carousel_song_title">${songs[index].title}</div>
+  <div class="carousel_song_artist">${songs[index].artist}</div>`;
+
+    let songAvailability = songs[index].availability_date
+      ? `
+      <div class="carousel_song_available">
+        <i class="fa-regular fa-calendar"></i>
+        AVAILABLE ON ${songs[index].availability_date}
+      </div>`
+      : ``;
+
+    let songFooter = `</div>`;
+
+    let songInnerHTML =
+      songHeader +
+      songCover +
+      songLink +
+      songInfo +
+      songAvailability +
+      songFooter;
+
+    document.getElementById("carousel_items").innerHTML += songInnerHTML;
+  }
+};
+
+populateCarousel();
